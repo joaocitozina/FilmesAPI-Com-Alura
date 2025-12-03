7af2d8f8e36402963b2c9704ee3958f12e517fd5
Joao Vitor Alves <joao.vitor_br@hotmail.com>
2025-12-02 21:43:59 -0300

    Commit inicial do projeto API Filmes com documentacao Swagger

---
+++ Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        
+        public string Titulo { get; set; }
+        
+        public string Genero { get; set; }
+       
+        public int Duracao { get; set; }
+
+        public DateTime HoraConsulta { get; set; } = DateTime.Now;
+    }
+}
